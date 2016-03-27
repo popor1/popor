7b1cee84de1634e9d747c2c267a4982be87ff02b
seongsu <97575756@navre.com>
2016-03-28 01:23:32 +0900

    저장

---
+++ Assets/Script/GameScript.js
@@ -0,0 +1,301 @@
+﻿
+var cols : int = 6;	
+var rows : int = 6;	
+var totalBlocks : int = cols * rows;
+
+var blockColor : int = 5;
+
+//var BLOCKBLANK : int = 0;
+
+var count : int = 0;
+
+/*
+var matchesNeededToWin : int = totalCards * 0.5;
+var matchesMade : int = 0;
+*/
+
+var blockW : int = 80;	
+var blockH : int = 80;	
+
+public var aBlock : Array;		
+var aGrid : Array;		
+var aBlocklist : ArrayList;
+
+var playerCanClick : boolean;	//flag to prevent the player from clicking buttons
+var playerMouseX : int;
+var playerMouseY : int;
+
+var playerHasWon : boolean = false;
+
+class Block extends System.Object{
+	//var isFaceUp : boolean = false;
+	
+	var BLOCKBLANK :  int = 0;
+	var BLOCKRED : int = 1;
+	var BLOCKBLUE : int = 2;
+	var BLOCKYELLOW : int = 3;
+	
+	var isMatched : boolean = false;
+	
+	var playerBlockClick : boolean = false;
+	
+	var img : String;
+	var id : int;
+	
+	function Block(){
+		img = "block";
+	}
+	function Block(img:String){
+		this.img = img;
+	}
+	function Block(img:String, id:int){
+		this.img = img;
+		this.id = id;
+	}
+}
+
+function Start () {
+	playerCanClick = true;	
+	
+	aBlock = new Array();	
+	aGrid = new Array();
+	BlockBuild();
+	
+	//Debug.Log(aBlock.length);
+	
+	for(var i = 0; i<rows; i++)
+	{
+		aGrid[i] = new Array();	
+		for(var j = 0; j<cols; j++)
+		{
+			var someNum:int = Random.Range(0, aBlock.length);
+			aGrid[i][j] = aBlock[someNum];
+			Debug.Log(someNum);
+			aBlock.RemoveAt(someNum);
+		}
+	}
+	
+	//DeleteCount();
+	
+
+}
+
+
+function OnGUI () {
+	GUILayout.BeginArea(Rect(0, 0, Screen.width, Screen.height));
+	BuildGrid();
+	if(playerHasWon){
+		BuildWinPrompt();
+	}
+	GUILayout.EndArea();
+}
+
+function BuildGrid(){
+	GUILayout.BeginVertical();
+	//GUILayout.FlexibleSpace();
+	var i : int;
+	var j : int;
+	
+	for(i=0; i<rows; i++)
+	{
+		GUILayout.BeginHorizontal();
+		//GUILayout.FlexibleSpace();
+		for(j=0; j<cols; j++)
+		{
+			var block : Object = aGrid[i][j];
+			var img : String;
+			
+			if(block.isMatched){
+				img = "block";
+			}
+			GUI.enabled = !block.isMatched;
+			
+			if(GUILayout.Button(Resources.Load(block.img), GUILayout.Width(blockW)))
+			{
+				//Debug.Log(empty_block.img);
+				DeleteCount(block);
+			}
+			GUI.enabled = true;
+		}
+		//GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal();
+	}
+	//GUILayout.FlexibleSpace();
+	GUILayout.EndVertical();
+}
+
+function BlockBuild()
+{
+	var totalBlock:int = 3;
+
+	//var blockIndex:int;
+	var block:Object;
+	var id:int = 0;
+	
+	for(i = 0; i<12; i++)
+	{
+		for(j= 0; j<totalBlock; j++)
+		{
+			block = new Block("block"+(j+1),id);
+			aBlock.Add(block);
+		}
+		id++;
+		//var aBlockColors:Array = ["Red", "Yellow", "Green", "White", "Blue", "Black"];
+		
+		//var someNum:int = Random.Range(0, aBlockColors.length);
+		//var theBlockColorName:String = aBlockColors[someNum];
+	}
+}
+
+
+
+function DeleteCount(block : Block)
+{
+	var checkBlock : String;
+	
+	block.playerBlockClick = true;
+	checkBlock = block.img;
+	//Debug.Log("확인");
+	
+	if(checkBlock == "block1")
+	{
+		block.isMatched = true;
+		DeleteBlock(playerMouseX, playerMouseY);
+		//DeleteBlock(playerMouseX, playerMouseY, checkBlock);
+		//Debug.Log("확인");
+	}
+	//block.isMatched = true;
+	//block.img = "block1";
+
+}
+
+function NodeCount()
+{
+	count = 0;
+	return count;
+	
+}
+
+function DeleteBlock(x:int , y:int)
+{
+	var arrayBlock : Array;
+	var img : String;
+	for( i = 0; i < 5; i++){
+		arrayBlock[i] = new Array();
+		for( j = 0; j<5; j++)
+		{	arrayBlock[i][j] =0;}
+	}
+	img = aGrid[x][y];
+	
+	if(img==aGrid[x+1][y])
+	{
+		DeleteBlock(x+1, y);
+	}
+	
+	if(img==aGrid[x][y+1])
+	{
+		DeleteBlock(x, y+1);
+	}
+	
+	if(img==aGrid[x-1][y])
+	{
+		DeleteBlock(x-1, y);
+	}
+	
+	if(img==aGrid[x][y-1])
+	{
+		DeleteBlock(x, y-1);
+	}
+	
+	Debug.Log(img);
+	
+	
+}
+/*
+function DeleteBlock(x:int , y:int, a:String)
+{
+	
+	//Debug.Log(x);
+	//Debug.Log(y);
+	
+	if(a==aBlocklist[x][y])
+	{
+		DeleteBlock(x+1, y, a);
+	}
+	
+	
+	
+}
+*/
+
+function Update()
+{
+	
+	playerMouseX = (Input.mousePosition.x)/80;
+	playerMouseY = (Input.mousePosition.y)/80;
+}
+
+
+
+function BuildWinPrompt(){
+	var winPromptW : int = 100;
+	var winPromptH : int = 90;
+	
+	var halfScreenW : float = Screen.width*0.5;
+	var halfScreenH : float = Screen.height*0.5;
+	
+	var halfPromptW : int = winPromptW*0.5;
+	var halfPromptH : int = winPromptH*0.5;
+	
+	GUI.BeginGroup(Rect(halfScreenW-halfPromptW, halfScreenH-halfPromptH, winPromptW, winPromptH));
+	GUI.Box (Rect(0, 0, winPromptW, winPromptH), "A Winner is You!!");
+	
+	if(GUI.Button(Rect(10, 40, 80, 20), "Play Again"))
+	{
+		//Application.LoadLevel("Title");
+	}
+	GUI.EndGroup();
+}
+
+/*
+function randomBlock()
+{
+	var randNum = Random.Range(1, blockColor);
+	return randNum;
+}
+
+
+function FlipCardFaceUp(card : Card){
+	card.isFaceUp = true;
+	//aCardsFlipped.Add(card);
+	if(aCardsFlipped.IndexOf(card) < 0)
+	{
+		aCardsFlipped.Add(card);
+		if(aCardsFlipped.Count == 2)
+		{
+			playerCanClick = false;
+			
+			yield WaitForSeconds(0.5);
+			if(aCardsFlipped[0].id == aCardsFlipped[1].id)
+			{
+				aCardsFlipped[0].isMatched = true;
+				aCardsFlipped[1].isMatched = true;
+				
+				matchesMade++;
+				if(matchesMade >= matchesNeededToWin){
+					playerHasWon = true;
+				}
+			}
+			else
+			{
+				aCardsFlipped[0].isFaceUp = false;
+				aCardsFlipped[1].isFaceUp = false;
+				//card.isMatched = false;
+			}	
+			aCardsFlipped = new ArrayList();
+			
+			playerCanClick = true;
+		}
+	}
+}
+*/
---
+++ Assets/Script/TestScript.js
@@ -0,0 +1,242 @@
+﻿//#pragma strict
+var cols = 5;	
+var rows = 8;	
+
+var blockW = 50;	
+var blockH = 50;	
+
+var aBlock;
+var aGrid;
+
+var bPlayerCanClick = false;
+var playerMouseX = 0;
+var playerMouseY = 0;
+
+var puzzleColor = 5;
+static var score : int = 0;
+var scoreCount = 0;
+
+var blockBlank : Texture2D;
+var blockBattery : Texture2D;
+var blockJewelry : Texture2D;
+var blockFire : Texture2D;
+var blockGear : Texture2D;
+var blockGold : Texture2D;
+
+var BLANKBLOCK = 0;
+var BATTERYBLOCK = 1;
+var JEWELRYBLOCK = 2;
+var FIREBLOCK = 3;
+var GEARBLOCK = 4;
+var GOLDBLOCK = 5;
+
+function Awake()
+{
+    
+    blockBlank = Resources.Load("BlankBlock");
+    blockBattery = Resources.Load("BatteryBlock");
+    blockJewelry = Resources.Load("FireBlock(1)");
+    blockFire = Resources.Load("GearBlock");
+    blockGear = Resources.Load("GoldBlock");
+    blockGold = Resources.Load("JewelryBlock"); 
+
+    //var instance : GameObject = Instantiate(Resources.Load("D:/study/TermProject/puzzle_test/Assets/Resources/block", GameObject));
+}
+
+function Start()
+{
+    //scroe = 0;
+    aBlock = new Array();
+    aGrid = new Array();
+
+    for(var i = 0; i< cols; i++)
+    {
+        aBlock[i] = new Array();
+        for(var j = 0; j<rows; j++)
+        {
+            var someNum = Random.Range(1, puzzleColor+1);
+            aBlock[i][j] = someNum;
+        }
+    }
+    
+    
+}
+function OnGUI()
+{
+    GUILayout.BeginArea(Rect(0, 0, Screen.width, Screen.height));
+    GUI.Label(Rect(800, 10, 200, 40), ("Score : " + score));
+    BuildPuzzle();
+    GUILayout.EndArea();
+}
+
+
+function BuildPuzzle()
+{
+    GUILayout.BeginVertical();
+    for(i = 0; i<cols; i++)
+    {
+        GUILayout.BeginHorizontal();
+        for(j = 0; j<rows ; j++)
+        {
+            switch(aBlock[i][j])
+            {
+                case BLANKBLOCK :
+                    GUILayout.Button(blockBlank, GUILayout.Width(blockW));
+                    break;
+                case BATTERYBLOCK :
+                    GUILayout.Button(blockBattery, GUILayout.Width(blockW));
+                    break;
+                case JEWELRYBLOCK :
+                    GUILayout.Button(blockJewelry, GUILayout.Width(blockW));
+                    break;
+                case FIREBLOCK :
+                    GUILayout.Button(blockFire , GUILayout.Width(blockW));
+                    break;
+                case GEARBLOCK :
+                    GUILayout.Button(blockGear , GUILayout.Width(blockW));
+                    break;
+                case GOLDBLOCK :
+                    GUILayout.Button(blockGold , GUILayout.Width(blockW));
+                    break;
+             }
+        }
+        GUILayout.EndHorizontal();
+    }
+    GUILayout.EndVertical();
+}
+function onMouseDown()
+{
+    if(Input.GetMouseButtonDown(0))
+    {
+        Debug.Log("들어옴");
+        bPlayerCanClick = true;
+    }
+}
+function Update()
+{
+    playerMouseX = (Input.mousePosition.x) / 50;
+    playerMouseY = (Screen.height-Input.mousePosition.y) / 50;
+   // var xpos = Input.mousePosition.x; 
+    //var ypos = Screen.height - Input.mousePosition.y; 
+   // var result = Mathf.Floor(xpos / gridpixels) + Mathf.Floor(ypos / gridpixels)*gidxsquares + 1;
+   // Debug.Log(ypos);
+    onMouseDown();
+}
+/*
+function BlockSwap(x, y, z)
+{
+    var tmp = z;
+    var SelectBlock1 = aBlock[x][y];
+
+}
+*/
+function BlockDown()
+{
+    var tmp = BLANKBLOCK;
+    for(var z = 0; z<rows; z++)
+    {
+        for(var i = 0; i<rows; i++)
+        {
+            for(var j = rows; j>0; j--)
+            {
+                if(aBlock[i][j] != BATTERYBLOCK  && aBlock[i][j] != JEWELRYBLOCK  && aBlock[i][j] != FIREBLOCK && aBlock[i][j] != GEARBLOCK  && aBlock[i][j] != GOLDBLOCK  )
+                {
+                    if(aBlock[i][j] == BLANKBLOCK)
+                    {
+                        tmp = aBlock[i][j-1];
+                        aBlock[i][j-1] = aBlock[i][j];
+                        aBlock[i][j] = tmp;
+                    }
+                }
+            }
+        }
+    }
+    BuildPuzzle();
+    //Block_Regenerate();
+}
+/*
+function Score()
+{
+    scroe = 0;
+    scoreCount = 0;
+    for(var i = 0; i<cols; i++)
+    {
+       
+        for(var j = 0; j<rows ; j++)
+        {
+            scoreCount = 0;
+            if(aBlock[i][j] == BLOCKWALL)
+            {
+                scoreCount = 100;
+            }
+            score += scoreCount;
+        }
+        
+    }
+    return score;
+}
+
+function DeleteBlock(x, y)
+{
+    bPlayerCanClick = false;
+    clickBlockColor = aBlock[x][y];
+    if(clickBlockColor!=BLOCKBLANK)
+    {
+        switch(clickBlockColor)
+        {
+            case BLOCKRED:
+                DeleteCount();
+                Score();
+                break;
+            case BLOCKBLUE:
+                DeleteCount();
+                Score();
+                break;
+            case BLOCKYELLOW:
+                DeleteCount();
+                Score();
+                break;
+            case BLOCKBLACK:
+                DeleteCount();
+                Score();
+                break;
+        }
+    }
+}
+
+function DeleteCount()
+{
+    count = 0;
+    DeletePredictionBlock(playerMouseY,  playerMouseX);
+    return count;
+}
+
+function DeletePredictionBlock(x, y)
+{
+    var nowBlock = aBlock[x][y];
+
+    aBlock[x][y] = BLOCKBLANK;
+
+    count++;
+
+    if(nowBlock==aBlock[x+1][y])
+    {
+        DeletePredictionBlock(x+1, y);
+    }
+	
+    if(nowBlock==aBlock[x][y+1])
+    {
+        DeletePredictionBlock(x, y+1);
+    }
+	
+    if(nowBlock==aBlock[x-1][y])
+    {
+        DeletePredictionBlock(x-1, y);
+    }
+	
+    if(nowBlock==aBlock[x][y-1])
+    {
+        DeletePredictionBlock(x, y-1);
+    }
+}
+*/
